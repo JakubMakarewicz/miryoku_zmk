98f332b55d35d01a8ba341b4d81dfcfb8b268dd8
JakubMakarewicz <78924900+JakubMakarewicz@users.noreply.github.com>
2023-05-23 19:42:22 +0000

    Create build-corne-nice_nano_v2_2.yml

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -337,7 +337,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 #define MIRYOKU_ALTERNATIVES_MEDIA \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &out OUT_BLE,      &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &out OUT_USB,      &kp C_PREV,        &kp C_RW,          &kp C_FF,          &kp C_NEXT,        \
-U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              U_EP_TOG,          &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_BRI_DN,      &kp C_BRI_UP       \
+U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              U_EP_TOG,          &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_BRI_DN,      &kp C_BRI_UP,       \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
 
 
